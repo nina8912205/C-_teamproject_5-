40f1cc8d1edd9774c313b9659190e1ed9a410d2f
light11014 <83333513+light11014@users.noreply.github.com>
2021-05-10 18:05:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,15 @@
 
 int main(void)
 {
+	int stage;  // 단계
+	system("mode con: cols=120 lines=43"); // 콘솔창 크키 조정
+	
 	/*====================== 게임 시작 =======================*/
+	game_title();  // 게임 타이틀 출력
+	stage = game_start_menu();  // 단계 선택
+	move_stage(stage);	    // 단계로 이동
+	if (stage != 23)            // 이름 입력 받기
+		get_name();
 	
 	/*======================= 게임진행 =======================*/
 	if (level == 1)

d8a3cb6fb1d6c4424de817cd78714d6a7caf5da9
light11014 <83333513+light11014@users.noreply.github.com>
2021-05-10 18:21:26 +0900

    Add files via upload

---
+++ game_title.c
@@ -0,0 +1,49 @@
+#include "common.h"
+
+void game_title()
+{
+	int ch;
+
+	gotoxy(0, 3);
+	printf("\t\t            ■■                      ■■\n");
+	printf("\t\t            ■  ■    ■■■■■    ■  ■                 ■■■■■    ■    ■\n");
+	printf("\t\t            ■    ■■          ■■    ■                     ■      ■  ■  ■\n");
+	printf("\t\t            ■  ■■              ■■  ■                 ■■  ■■  ■  ■■■\n");
+	printf("\t\t        ■■■■        ■    ■      ■■■■                 ■      ■  ■  ■\n");
+	printf("\t\t    ■■  ■            ■    ■          ■  ■■         ■■■■■    ■    ■\n");
+	printf("\t\t  ■      ■      ■      ■  ■  ■      ■      ■                   ■      ■ \n");
+	printf("\t\t■      ■      ■          ■      ■      ■      ■     ■■■■■  ■      ■\n");
+	printf("\t\t■      ■                                  ■      ■             ■  ■■■■■\n");
+	printf("\t\t  ■■■            ■            ■          ■■■       ■■■■■  ■      ■\n");
+	printf("\t\t      ■          ■■          ■■          ■           ■          ■      ■\n");
+	printf("\t\t      ■          ■■          ■■          ■           ■■■■■  ■■■■■\n");
+	printf("\t\t    ■                  ■  ■                  ■           \n");
+	printf("\t\t    ■            ■■■■■■■■■            ■           ■    ■  ■■■  ■\n");
+	printf("\t\t    ■        ■■■              ■■■        ■         ■■■  ■      ■  ■\n");
+	printf("\t\t    ■    ■■        ■      ■        ■■    ■                 ■      ■  ■\n");
+	printf("\t\t    ■    ■            ■■■            ■    ■           ■    ■      ■  ■\n");
+	printf("\t\t      ■■                                  ■■           ■  ■  ■  ■■■  ■\n");
+	printf("\t\t      ■                                      ■           ■  ■  ■■■      ■■\n");
+	printf("\t\t      ■                                      ■           ■  ■  ■  ■      ■\n");
+	printf("\t\t        ■                                  ■■■■       ■  ■  ■  ■      ■\n");
+	printf("\t\t        ■                                  ■      ■     ■  ■  ■  ■      ■\n");
+	printf("\t\t        ■■                              ■        ■       ■    ■  ■■■  ■\n");
+	printf("\t\t          ■■                          ■          ■\n");
+	printf("\t\t              ■■                  ■■          ■       ■      ■  ■      ■\n");
+	printf("\t\t              ■■■■■■■■■■■■          ■         ■      ■  ■      ■\n");
+	printf("\t\t            ■  ■                  ■        ■           ■    ■    ■      ■\n");
+	printf("\t\t          ■    ■       K  U       ■      ■             ■  ■      ■      ■\n");
+	printf("\t\t        ■    ■  ■■          ■■  ■  ■               ■■        ■      ■\n");
+	printf("\t\t      ■      ■      ■      ■      ■■                 ■  ■      ■      ■\n");
+	printf("\t\t    ■      ■          ■  ■          ■                 ■    ■    ■      ■\n");
+	printf("\t\t  ■        ■            ■            ■                 ■      ■  ■      ■\n");
+	printf("\t\t  ■        ■                          ■                 ■      ■  ■■■■■\n");
+	printf("\t\t  ■      ■■                          ■\n");
+	printf("\t\t    ■■■  ■                          ■               < PRESS ANY KEY TO START >\n");
+	printf("\n\n");
+	printf("\t\t\t\t\t(0451 5팀) 김현욱 노민영 오색빛 정예원\n\n");
+
+	ch = _getch();
+	if (ch)
+		system("cls");      // 아무키나 입력 -> 화면 글자 지우기
+}

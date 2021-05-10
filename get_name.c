ceebfc17958a18a1a36dc7ca22dfb216afc7d37d
light11014 <83333513+light11014@users.noreply.github.com>
2021-05-10 18:21:46 +0900

    Add files via upload

---
+++ get_name.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+void get_name()
+{
+	static char name[256];
+
+	gotoxy(47, 21);
+	printf("네 이름이 뭐니? \n");
+	gotoxy(63, 21);
+	scanf("%s", &name);
+	gotoxy(47, 23);
+	printf("%s ! 멋진 이름이다!", name);
+}

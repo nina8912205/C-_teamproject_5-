dde0c8c466c25f3d829dd95cd83a9d678419bfab
nina8912205 <71542788+nina8912205@users.noreply.github.com>
2021-05-10 14:02:38 +0900

    Create common.h
    common 헤더파일입니다.

---
+++ common.h
@@ -0,0 +1,11 @@
+# include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*게임 진행 함수*/
+int elementary(); // 초등학교
+
+
+/* 아이템 함수 */
+void item_random(void);   // 아이템 랜덤 지급 함수
+void item_board(void);       // 아이템 보유 현황 함수

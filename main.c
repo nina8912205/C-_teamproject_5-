ffdc300cca2ef509580aad13bd188ff18eb9cd3e
light11014 <83333513+light11014@users.noreply.github.com>
2021-05-10 22:10:38 +0900

    Update main.c

--- main.c
+++ main.c
@@ -6,11 +6,14 @@ int main(void)
 	system("mode con: cols=120 lines=43"); // 콘솔창 크키 조정
 	
 	/*====================== 게임 시작 =======================*/
-	game_title();               // 게임 타이틀 출력
-	stage = game_start_menu();  // 단계 선택
-	move_stage(stage);	    // 단계로 이동
-	if (stage != GAMERULES)            // 이름 입력 받기
-		get_name();
+	game_title();                  // 게임 타이틀 출력
+	stage = game_start_menu();     // 단계 선택
+	move_stage(stage);	       // 단계로 이동
+	if (stage != GAMERULES)            
+		get_name();            // 이름 입력 받기
+	Sleep(1500);                   // 1.5초 기다리기
+	srand((unsigned)time(NULL));  
+	item_random();                 // 아이템 뽑기
 	
 	/*======================= 게임진행 =======================*/
 	if (level == 1)
@@ -52,10 +55,7 @@ int main(void)
 
 
 
-	/* ======================= 아이템 ========================*/
 
-	srand((unsigned)time(NULL));
-	item_random();
 
 }
 void gotoxy(int x, int y)

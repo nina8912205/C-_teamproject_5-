a51595bf4e9f2c32f07b728c5d4c14216a87dc8b
light11014 <83333513+light11014@users.noreply.github.com>
2021-05-12 14:08:07 +0900

    Update common.h

--- common.h
+++ common.h
@@ -25,7 +25,7 @@ int game_start_menu();      // 게임 시작 메뉴 출력, 난이도 선택
 void move_stage(int stage); // 선택한 난이도로 이동
 void get_name();            // 이름 입력
 void game_rules();          // 게임 방법 출력
-void item_random();         // 아이템 뽑기
+
 
 /* 게임 진행 함수 */
 int elementary(); // 초등학교
@@ -35,10 +35,13 @@ int middle();  //중학교
 int high();  //고등학교
 // 수능
 
+
 /* 아이템 함수 */
-void item_random(void);   // 아이템 랜덤 지급 함수
-void item_board(void);       // 아이템 보유 현황 함수
+void item_random();         // 아이템 뽑기
+
 
 /* 콘솔창 편집 함수 */
-void gotoxy(int x, int y); // 커서 이동 함수
+void gotoxy(int x, int y);  // 커서 이동 함수
+void drawline();            // 화면 테두리 그리기
+void drawitem();            // 아이템, 목숨 출력
 
